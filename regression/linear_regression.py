291dc0d3db4fe364d2c9c31e7f68d5dca88825d7
Pham Van Toan <phamvantoan1993@gmail.com>
2016-11-24 15:37:26 +0700

    Linear Regression House Pricing

---
+++ regression/linear_regression.py
@@ -0,0 +1,66 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn import linear_model
+from sklearn.metrics import mean_absolute_error
+from sklearn.externals import joblib
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
+def getData():
+    # Get home data from CSV file
+    dataFile = None
+    if os.path.exists('home_data.csv'):
+        print("-- home_data.csv found locally")
+        dataFile = pd.read_csv('home_data.csv', skipfooter=1)
+
+    return dataFile
+
+def linearRegressionModel(X_train, Y_train, X_test, Y_test):
+    linear = linear_model.LinearRegression()
+    # Training process
+    linear.fit(X_train, Y_train)
+    # Evaluating the model
+    score_trained = linear.score(X_test, Y_test)
+
+    return score_trained
+
+def lassoRegressionModel(X_train, Y_train, X_test, Y_test):
+    lasso_linear = linear_model.Lasso(alpha=1.0)
+    # Training process
+    lasso_linear.fit(X_train, Y_train)
+    # Evaluating the model
+    score_trained = lasso_linear.score(X_test, Y_test)
+
+    return score_trained
+
+if __name__ == "__main__":
+    data = getData()
+    if data is not None:
+        # Selection few attributes
+        attributes = list(
+            [
+                'num_bed',
+                'year_built',
+                'num_room',
+                'num_bath',
+                'living_area',
+            ]
+        )
+        # Vector price of house
+        Y = data['askprice']
+        # print np.array(Y)
+        # Vector attributes of house
+        X = data[attributes]
+        # Split data to training test and testing test
+        X_train, X_test, Y_train, Y_test = train_test_split(np.array(X), np.array(Y), test_size=0.2)
+        # Linear Regression Model
+        linearScore = linearRegressionModel(X_train, Y_train, X_test, Y_test)
+        print 'Linear Score = ' , linearScore
+        # LASSO Regression Model
+        lassoScore = lassoRegressionModel(X_train, Y_train, X_test, Y_test)
+        print 'Lasso Score = ', lassoScore
